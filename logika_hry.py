ab184cc247aec6c1699e1156d6c47c050fc2a739
lukas683 <lukas.radacovsky.po@gmail.com>
2025-04-01 18:36:38 +0200

    Vytvorenie zoznamu na ukladanie hodov.

--- logika_hry.py
+++ logika_hry.py
@@ -1,4 +1,15 @@
-import graphics
+# -*- coding: utf-8 -*-
 
 
+class LogikaHry:
+    CERVENA=1
+    MODRA=2
+    PRAZDNO=0
+
+
+    zoznam_policok=[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+
+
+    def __init__(self):
+        pass
 
